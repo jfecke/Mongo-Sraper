1ba0cba04fc728364e0272c104c2eb1dc05bd7e9
jfecke <jfecke@gmail.com>
2019-02-28 18:33:03 -0600

    Made delete button

--- public/app.js
+++ public/app.js
@@ -3,8 +3,8 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>"+ "<strong>" + data[i].title + "/strong" +
-    "<br />" + "<a href='"+ data[i].link + "'>" + data[i].link+  "<target='_blank' /a>" + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>"+ "<strong>" + data[i].title + "</strong>" +
+    "<br />" + "<strong>Summary: </strong>"+ data[i].summary + "<br />" + "<a href='"+ data[i].link + "'>" + data[i].link+  "<target='_blank' /a>" + "</p>");
   }
 });
 
@@ -32,6 +32,7 @@ $(document).on("click", "p", function() {
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
 
       // If there's a note in the article
       if (data.note) {
@@ -57,6 +58,34 @@ $(document).on("click", "#savenote", function() {
       title: $("#titleinput").val(),
       // Value taken from note textarea
       body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+$(document).on("click", "#deletenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: "",
+      // Value taken from note textarea
+      body: ""
     }
   })
     // With that done
